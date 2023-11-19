c244926f960b1087c53a6a4d660c40aa2883286b
christopherschmid <46402467+christopherschmid@users.noreply.github.com>
2023-11-19 15:07:41 -0400

    Add files via upload

---
+++ RDENGLISCH/countdown.js
@@ -0,0 +1,21 @@
+// Countdown to Remembrance Day
+const countdownDate = new Date('November 11, 2024 00:00:00').getTime();
+
+const countdown = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = `
+        ${days}d ${hours}h ${minutes}m ${seconds}s
+    `;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = 'Remembrance Day is here!';
+    }
+}, 1000);
